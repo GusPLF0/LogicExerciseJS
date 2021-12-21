6939f134c97b72d88e4d72e91734ff1e92c2a528
Gustavo Pedro Lima <gpedr56@gmail.com>
2021-12-21 20:07:11 -0300

    Update script.js

--- Who_is_bigger/script.js
+++ Who_is_bigger/script.js
@@ -1,15 +1,9 @@
 // Script for comparing 2 numbers and log who is bigger
 
 
-function whoIsBigger(num1, num2){
-    if(num1>num2){
-        return `O maior é: ${num1}`;
-    }else if(num1<num2){
-        return `O maior é: ${num2}`;
-    }else{
-        return `Os dois número são iguais`;
-    }
+function whoIsBigger(...values){
+    return `O maior é: ${Math.max(...values)}`
 }
 
 
-console.log(whoIsBigger(3,4))
+console.log(whoIsBigger(11,10, 13, 19, 22))
