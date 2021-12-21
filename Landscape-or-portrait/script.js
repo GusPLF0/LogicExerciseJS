effaad153b30591c0d9d14369cb83c73402a5249
Gustavo Pedro Lima <gpedr56@gmail.com>
2021-12-21 20:33:57 -0300

    Update script.js

--- Landscape-or-portrait/script.js
+++ Landscape-or-portrait/script.js
@@ -1,3 +1,5 @@
+//Script to compare, based on an image metric, whether you have a portrait or landscape image format
+
 landtrait = (width, height) => (height<width)?"LandScape":"Portrait";
 
 console.log(landtrait(1080,1920));
